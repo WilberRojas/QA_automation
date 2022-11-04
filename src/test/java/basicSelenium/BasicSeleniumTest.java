bdca1b8d8103e601872c18fe57a767896563895f
WilberRojas <74274632+WilberRojas@users.noreply.github.com>
2022-11-04 19:14:41 -0400

    update tasks

--- src/test/java/basicSelenium/BasicSeleniumTest.java
+++ src/test/java/basicSelenium/BasicSeleniumTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.lang.Thread;
 import java.util.Date;
+import org.openqa.selenium.Keys;
 
 public class BasicSeleniumTest {
 
@@ -21,19 +23,32 @@ public class BasicSeleniumTest {
         driver.get("http://todo.ly/");
     }
 
-    //@AfterEach
+    @AfterEach
     public void cleanup(){
         driver.quit();
     }
 
     @Test
     public void verifyCRUDProject() throws InterruptedException {
-        String nameProject="Wil_"+new Date().getTime();
+        // login
+        login();
+
+        // create
+        String nameProject="Wil"+new Date().getTime();
+        create_project(nameProject);
+
         String nameTask1="Take over the world";
         String nameTask2="Be happy";
         String nameTask3="Buy bread, milk, eggs";
 
-        // login
+        create_task(nameTask1);
+        create_task(nameTask2);
+        create_task(nameTask3);
+
+        edit_task(nameTask1, nameTask1+" (changed)");
+    }
+
+    public void login() throws InterruptedException{
         driver.findElement(By.xpath("//img[contains(@src,'pagelogin')]")).click();
         driver.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxEmail")).sendKeys("bootcamp@mojix44.com");
         driver.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword")).sendKeys("12345");
@@ -41,9 +56,9 @@ public class BasicSeleniumTest {
         Thread.sleep(1000);
         Assertions.assertTrue(driver.findElement(By.id("ctl00_HeaderTopControl1_LinkButtonLogout")).isDisplayed()
                 ,"ERROR login was incorrect");
+    }
 
-        // create
-
+    public void create_project(String nameProject) throws InterruptedException{
         driver.findElement(By.xpath("//td[text()='Add New Project']")).click();
         driver.findElement(By.id("NewProjNameInput")).sendKeys(nameProject);
         driver.findElement(By.id("NewProjNameButton")).click();
@@ -51,20 +66,22 @@ public class BasicSeleniumTest {
         int actualResult=driver.findElements(By.xpath(" //td[text()='"+nameProject+"'] ")).size();
         Assertions.assertTrue(actualResult >= 1
                 ,"ERROR The project was not created");
+    }
 
-        //Task 1
-        driver.findElement(By.id("NewItemContentInput")).sendKeys(nameTask1);
-        driver.findElement(By.id("NewItemAddButton")).click();
-        Thread.sleep(1000);
-
-        //Task 2
-        driver.findElement(By.id("NewItemContentInput")).sendKeys(nameTask2);
-        driver.findElement(By.id("NewItemAddButton")).click();
-        Thread.sleep(1000);
-
-        //Task 3
-        driver.findElement(By.id("NewItemContentInput")).sendKeys(nameTask3);
+    public void create_task(String nameTask) throws InterruptedException{
+        driver.findElement(By.id("NewItemContentInput")).sendKeys(nameTask);
         driver.findElement(By.id("NewItemAddButton")).click();
         Thread.sleep(1000);
     }
+
+    public void edit_task(String nameTask, String updateTask) throws InterruptedException{
+        WebElement variable1 = driver.findElement(By.xpath(" //div[text()='"+nameTask+"'] "));
+        String id1 = variable1.getAttribute("itemid");
+
+        driver.findElement(By.xpath(" //td/div[@itemid='"+id1+"'] ")).click();
+        driver.findElement(By.xpath(" //textarea[@itemid='"+id1+"']")).clear();
+        driver.findElement(By.xpath(" //textarea[@itemid='"+id1+"']")).sendKeys(updateTask);
+        driver.findElement(By.xpath(" //textarea[@itemid='"+id1+"']")).sendKeys(Keys.RETURN);
+    }
+
 }
